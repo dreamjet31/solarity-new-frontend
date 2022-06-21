85e6ece2a4c4623dec780555a07cc8cad373aa93
jamesl0724 <jamesdream0724@gmail.com>
2022-06-22 00:43:35 +0700

    scroll

--- src/modules/Auth/GeneralInfo.tsx
+++ src/modules/Auth/GeneralInfo.tsx
@@ -18,7 +18,7 @@ export const GeneralInfo = () => {
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-2 mt-[20px] items-baseline">
-        {step == 0?<div className=" pr-[0] lg:pr-[5%]">
+        {step == 0?<div className=" pr-[0] lg:pr-[7%]">
           <div className="relative w-auto my-6 mx-auto">
             {/*content*/}
             <div className="rounded-[30px] min-h-[calc(100vh-100px)] shadow-lg relative flex flex-col w-full bg-[#141416] outline-none focus:outline-none">
@@ -62,10 +62,10 @@ export const GeneralInfo = () => {
             </div>
           </div>
         </div>:""}
-        {step == 1?<div className=" pr-[0] lg:pr-[5%]">
+        {step == 1?<div className=" pr-[0] lg:pr-[7%]">
           <div className="relative w-auto my-6 mx-auto">
             {/*content*/}
-            <div className="rounded-[30px] h-[calc(100vh-100px)] shadow-lg relative flex flex-col w-full bg-[#141416] outline-none focus:outline-none">
+            <div className="rounded-[30px] min-h-[calc(100vh-100px)] shadow-lg relative flex flex-col w-full bg-[#141416] outline-none focus:outline-none">
               {/*header*/}
               <div className="flex items-start justify-between pt-8 pl-[32px] pr-[32px] lg:p-14 lg:pb-0 lg:pr-12 rounded-t">
                 <h3 className="text-[28px] lg:text-[30px] text-white font-medium tracking-[0.02em]">
@@ -75,7 +75,10 @@ export const GeneralInfo = () => {
               </div>
               {/*body*/}
               <div className="relative p-[32px] lg:p-14 flex-auto">
-                <div className="grid grid-cols-2 xl:grid-cols-3">
+                <div className="grid grid-cols-2 xl:grid-cols-3 max-h-[510px] overflow-scroll">
+                  <div className="p-2">
+                    <DaoPanel imageSrc={DaoPicImg} backSrc={DaoBGImg} title="Solana Money Boys" />
+                  </div>
                   <div className="p-2">
                     <DaoPanel imageSrc={DaoPicImg} backSrc={DaoBGImg} title="Solana Money Boys" />
                   </div>
@@ -84,7 +87,7 @@ export const GeneralInfo = () => {
                   </div>
                 </div>
               </div>
-              <div className="w-full p-[32px] lg:p-14 flex-auto absolute bottom-0">
+              <div className="w-full px-[32px] py-[32px] lg:px-14 lg:py-8 flex-auto flex items-end">
                 <div className="inline-block w-[20%] pr-2">
                   <BackButton onClick={() => setStep(0)} styles="rounded-[15px]" />
                 </div>
@@ -95,10 +98,10 @@ export const GeneralInfo = () => {
             </div>
           </div>
         </div>:""}
-        {step == 2?<div className=" pr-[0] lg:pr-[5%]">
+        {step == 2?<div className=" pr-[0] lg:pr-[7%]">
           <div className="relative w-auto my-6 mx-auto">
             {/*content*/}
-            <div className="rounded-[30px] h-[calc(100vh-100px)] shadow-lg relative flex flex-col w-full bg-[#141416] outline-none focus:outline-none">
+            <div className="rounded-[30px] min-h-[calc(100vh-100px)] shadow-lg relative flex flex-col w-full bg-[#141416] outline-none focus:outline-none">
               {/*header*/}
               <div className="flex items-start justify-between pt-8 pl-[32px] pr-[32px] lg:p-14 lg:pb-0 lg:pr-12 rounded-t">
                 <h3 className="text-[28px] lg:text-[30px] text-white font-medium tracking-[0.02em]">
@@ -107,31 +110,57 @@ export const GeneralInfo = () => {
                 <AddressButton caption="Ak...VqT9" icon={AddressImg} onClick={null} />
               </div>
               <div className="relative p-[32px] lg:p-14 flex-auto">
-                <Dropzone onDrop={acceptedFiles => {console.log(acceptedFiles); setFiles(acceptedFiles.length);}}>
-                  {({getRootProps, getInputProps}) => (
-                    <div {...getRootProps()}>
-                      <input {...getInputProps()} />
-                      <label
-                          className="flex w-full h-24 px-4 transition bg-transparent border-2 border-white/20 border-dashed rounded-md appearance-none cursor-pointer hover:border-white/30 focus:outline-none">
-                          <span className="flex items-center space-x-2 mr-3">
-                            <Image src={GalleryImg} />
-                          </span>
-                          <span className="flex items-center space-x-2">
-                              {files?<span className="font-medium text-[#f3f3f3]">
-                                  <label className="text-primary">{files}</label> file&#40;s&#41; selected
-                                  <br></br>
-                                  <label className="text-[14px] text-white/30">Supports&#58; JPEG, JPEG2000, PNG</label>
-                              </span>:<span className="font-medium text-[#f3f3f3]">
-                                  Drop image here or&nbsp;<label className="text-primary">browse</label>
-                                  <br></br>
-                                  <label className="text-[14px] text-white/30">Supports&#58; JPEG, JPEG2000, PNG</label>
-                              </span>}
-                          </span>
-                      </label>
-                    </div>
-                  )}
-                </Dropzone>
-                <div className="grid grid-cols-2 xl:grid-cols-3 mt-5">
+                <div className="mb-10">
+                  <Dropzone onDrop={acceptedFiles => {console.log(acceptedFiles); setFiles(acceptedFiles.length);}}>
+                    {({getRootProps, getInputProps}) => (
+                      <div {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        <label
+                            className="flex w-full h-24 px-4 transition bg-transparent border-2 border-white/20 border-dashed rounded-md appearance-none cursor-pointer hover:border-white/30 focus:outline-none">
+                            <span className="flex items-center space-x-2 mr-3">
+                              <Image src={GalleryImg} />
+                            </span>
+                            <span className="flex items-center space-x-2">
+                                {files?<span className="font-medium text-[#f3f3f3]">
+                                    <label className="text-primary">{files}</label> file&#40;s&#41; selected
+                                    <br></br>
+                                    <label className="text-[14px] text-white/30">Supports&#58; JPEG, JPEG2000, PNG</label>
+                                </span>:<span className="font-medium text-[#f3f3f3]">
+                                    Drop image here or&nbsp;<label className="text-primary">browse</label>
+                                    <br></br>
+                                    <label className="text-[14px] text-white/30">Supports&#58; JPEG, JPEG2000, PNG</label>
+                                </span>}
+                            </span>
+                        </label>
+                      </div>
+                    )}
+                  </Dropzone>
+                </div>
+                <div className="grid grid-cols-2 xl:grid-cols-3 mt-5 max-h-[510px] overflow-scroll">
+                  <div className="p-2">
+                    <AvatarPanel imageSrc={ProfileImg} title="RESSURECTION..." />
+                  </div>
+                  <div className="p-2">
+                    <AvatarPanel imageSrc={ProfileImg} title="RESSURECTION..." />
+                  </div>
+                  <div className="p-2">
+                    <AvatarPanel imageSrc={ProfileImg} title="RESSURECTION..." />
+                  </div>
+                  <div className="p-2">
+                    <AvatarPanel imageSrc={ProfileImg} title="RESSURECTION..." />
+                  </div>
+                  <div className="p-2">
+                    <AvatarPanel imageSrc={ProfileImg} title="RESSURECTION..." />
+                  </div>
+                  <div className="p-2">
+                    <AvatarPanel imageSrc={ProfileImg} title="RESSURECTION..." />
+                  </div>
+                  <div className="p-2">
+                    <AvatarPanel imageSrc={ProfileImg} title="RESSURECTION..." />
+                  </div>
+                  <div className="p-2">
+                    <AvatarPanel imageSrc={ProfileImg} title="RESSURECTION..." />
+                  </div>
                   <div className="p-2">
                     <AvatarPanel imageSrc={ProfileImg} title="RESSURECTION..." />
                   </div>
@@ -140,12 +169,12 @@ export const GeneralInfo = () => {
                   </div>
                 </div>
               </div>
-              <div className="w-full p-[32px] lg:p-14 flex-auto absolute bottom-0">
+              <div className="w-full p-[32px] lg:p-14 flex-auto flex items-end px-[32px] py-[32px] lg:px-14 lg:py-8">
                 <div className="inline-block w-[20%] pr-2">
                   <BackButton onClick={() => setStep(1)} styles="rounded-[15px]" />
                 </div>
                 <div className="inline-block w-[80%] pl-2">
-                  <PrimaryButton caption="Continue" icon="" bordered={false} onClick={() => setStep(3)} disabled={false} styles="rounded-[15px]" />
+                  <PrimaryButton caption="Continue" icon="" bordered={false} onClick={null} disabled={false} styles="rounded-[15px]" />
                 </div>
               </div>
             </div>
