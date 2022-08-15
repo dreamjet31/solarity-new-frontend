7e07e683d3f30f9213fe39d4b5fd24b180071c78
shiningsea0415 <ski.britness@gmail.com>
2022-08-16 01:12:10 +0700

    set live event height

--- src/components/Common/Panels/LiveEventPanel.tsx
+++ src/components/Common/Panels/LiveEventPanel.tsx
@@ -8,10 +8,10 @@ const LiveEventPanel = (props) => {
     return (
         <div className="flex flex-col relative overflow-hidden cursor-pointer
                         w-full rounded-[20px] border-[1.2px] border-[#272829] hover:border-primary transition duration-300 bg-[#242424]" onClick={() => {}}>
-            <div className="w-full"><Image src={data.image} width="100%" height="70%" layout="responsive" /></div>
+            <div className="w-full"><Image src={data.image} width="100%" height="65%" layout="responsive" /></div>
             <div className="py-[8px] px-[20px] h-full flex flex-col justify-between">
-                <div className="text-[20px] text-[#F3F3F3] leading-[32px]">{data.title}</div>
-                <div className="flex items-center justify-between text-[#29B080] text-[14px] mt-[20px]">
+                <div className="text-[20px] text-[#F3F3F3] leading-[24px]">{data.title}</div>
+                <div className="flex items-center justify-between text-[#29B080] text-[14px] mt-[10px]">
                     <div className="flex flex-row items-center">
                         {
                             data.users.filter((user, index) => index < 3).map((user, index) => (
