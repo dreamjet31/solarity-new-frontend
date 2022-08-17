e2270727913ab63871c9ca3fcfa111b83441e8f8
shiningsea0415 <ski.britness@gmail.com>
2022-08-17 07:16:33 +0700

    refactor gradient

--- src/components/Common/Panels/LiveEventPanel.tsx
+++ src/components/Common/Panels/LiveEventPanel.tsx
@@ -14,7 +14,7 @@ const LiveEventPanel = (props) => {
                     <div className="h-[20px] w-[20px] rounded-full overflow-hidden"><Image src={data.creator.avatar} width={20} height={20} layout="responsive" /></div>
                     <span className="font-medium text-[12px] text-white">{data.creator.name}</span>
                 </div>
-                <div className="absolute bottom-0 left-0 h-[45px] w-full bg-gradient-to-b from-transparent via-[#04110cc5] via-3/5 to-[#04110C]"></div>
+                <div className="absolute bottom-0 left-0 h-[50px] w-full bg-gradient-to-b from-transparent from-20 via-[#04110cf1] via-60 to-[#04110C]"></div>
             </div>
             <div className="pb-[10px] pt-[5px] px-[10px] h-full flex flex-col justify-between">
                 <div className="text-[20px] text-[#F3F3F3] leading-[24px]">{data.title}</div>
