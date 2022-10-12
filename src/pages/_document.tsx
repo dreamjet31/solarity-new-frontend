72a6506ffb29f38c1498ed5460d0a3818d583359
memokey <skytopground@gmail.com>
2022-10-13 02:19:44 +0800

    changed logo part

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,10 +10,10 @@ class MyDocument extends Document {
           <link href='https://fonts.googleapis.com/css?family=Outfit' rel='stylesheet'></link>
 
           <link rel="shortcut icon" href="/favicon.png"></link>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
-          <link rel="manifest" href="/site.webmanifest"></link>
+          <link rel="apple-touch-icon" sizes="180x180" href="/logos/apple-touch-icon.png"></link>
+          <link rel="icon" type="image/png" sizes="32x32" href="/logos/favicon-32x32.png"></link>
+          <link rel="icon" type="image/png" sizes="16x16" href="/logos/favicon-16x16.png"></link>
+          <link rel="manifest" href="/logos/site.webmanifest"></link>
           <meta name="msapplication-TileColor" content="#da532c" />
         </Head>
         <body>
