0b250b22a6d94313f7e65b2364aa483ae0dc0857
memokey <skytopground@gmail.com>
2022-10-13 17:32:27 +0800

    fixed mobile design on avatar selection part

--- src/components/Experience/Common/ModalContent/AvatarSelectionContent.tsx
+++ src/components/Experience/Common/ModalContent/AvatarSelectionContent.tsx
@@ -12,9 +12,9 @@ const AvatarSelectionContent = () => {
   }))
 
   return (
-    <div className=" grid grid-cols-4 profile-settings-content tab-general-content h-full min-w-[330px] items-between gap-[32px] pt-[2px] pb-[2px] mb-[25px] overflow-y-auto overflow-x-visible items-center">
+    <div className=" grid grid-cols-4 xs:grid-cols-1 sm:grid-cols-4 profile-settings-content tab-general-content h-full min-w-[330px] items-between gap-[32px] pt-[2px] pb-[2px] mb-[25px] overflow-y-auto overflow-x-visible items-center">
       <div
-        className=" col-span-3 h-[400px] w-full rounded-[15px] bg-[#181818]"
+        className=" col-span-3 xs:col-span-2 sm:col-span-3 h-[400px] xs:h-[260px] sm:h-[400px] w-full rounded-[15px] bg-[#181818]"
       >
         <AvatarPanel
           modelPath={models[newModelIndex].modelUrl}
@@ -26,7 +26,7 @@ const AvatarSelectionContent = () => {
       {/* begin of avatar list------------------------------------------------------------------------------------------------ */}
       <div className="col-span-1 relative h-[400px] overflow-y-scroll overflow-x-visible overscroll-contain">
         <div
-          className="grid gap-[16px] w-full h-[100%] pr-1 pb-[30px] "
+          className="grid xs:grid-cols-2 sm:grid-cols-1 gap-[16px] w-full h-[100%] pr-1 pb-[30px] "
         >
           {PsuedoAvatarItemData.map((i, index) => {
             return (
