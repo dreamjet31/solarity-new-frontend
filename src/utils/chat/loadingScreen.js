a71319895d655f9ed84ce73d8bfdd9b81b1ceaa4
memokey <skytopground@gmail.com>
2022-10-18 17:34:18 +0800

    changed

--- src/utils/chat/loadingScreen.js
+++ src/utils/chat/loadingScreen.js
@@ -30,8 +30,9 @@ export function update_loadingScreen() {
     if (!!loadingScreenEl) loadingScreenEl.style.display = "none";
     if (sceneWrapperEl) sceneWrapperEl.style.opacity = 100;
   }, 60000);
-
+  
   modelsLoaded++;
+  console.log(modelsLoaded, modelsNumber);
   if (loadingBars && loadingStatus) {
     for(var i = 0; i < loadingBars.length; i ++) {
       loadingBars[i].children[1].style.setProperty('--percent', Math.round((modelsLoaded * 100) / modelsNumber));
