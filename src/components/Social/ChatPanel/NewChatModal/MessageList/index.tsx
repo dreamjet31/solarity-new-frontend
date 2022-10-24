166f953df424dc0544fc176649879dcff0301c5c
memokey <skytopground@gmail.com>
2022-10-25 04:23:29 +0800

    integrated new chat modal

---
+++ src/components/Social/ChatPanel/NewChatModal/MessageList/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { PrimaryButton } from "../../../../Common/Buttons";
+import TitleItem from "../../TitleItem";
+import MessageListItem from "./MessageListItem";
+
+const MessageList = (props) => {
+  const { selectedFriend, setSelectedFriend, suggestedFriends } = props;
+
+  const dispatch = useDispatch();
+
+  return (
+    <div className="pb-6">
+      <TitleItem title="Suggested" comment="" />
+      <div className="grid gap-y-3">
+        {!!suggestedFriends && suggestedFriends.map((friend, index) => (
+          <MessageListItem
+            title={friend.username}
+            image={
+              <img
+                src={friend.profileImage ? friend.profileImage : "/images/experience/psuedo_avatars/avatar.png"}
+                width={52}
+                height={52}
+              />
+            }
+            status={friend.onlineFlag}
+            selected={friend === selectedFriend}
+            gap={3}
+            key={index}
+            onSelect={() => setSelectedFriend(friend)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default MessageList;
--- src/components/Social/ChatPanel/index.tsx
+++ src/components/Social/ChatPanel/index.tsx
@@ -1,12 +1,19 @@
 import ChattingThreadBox from "components/Experience/RoomInterior/ChattingBox/ChattingThreadBox";
-import Input from "components/Experience/RoomInterior/ChattingBox/Input";
+import MsgInput from "components/Experience/RoomInterior/ChattingBox/MsgInput";
 import UploadDropZoneImg from "components/Experience/RoomInterior/ChattingBox/UploadDropZoneImg";
 import React, { useState } from "react";
+import { RootStateOrAny, useDispatch, useSelector } from "react-redux";
+import { setIsNewChatModal } from "redux/slices/chatSlice";
 import { RoundPlus } from "../../icons";
 import TitleItem from "./TitleItem";
 import UsersSidebar from "./UsersSidebar";
+import NewChatModal from './NewChatModal';
 
 const ChatPanel = () => {
+  const dispatch = useDispatch();
+  const { isNewChatModal } = useSelector((state: RootStateOrAny) => ({
+    isNewChatModal: state.chat.isNewChatModal
+  }));
   const [focusState, setFocusState] = useState(false);
   const [toggleDropZone, setToggleDropZone] = useState("none");
   const [newMsgDataState, setNewMsgDataState] = useState({
@@ -28,18 +35,18 @@ const ChatPanel = () => {
         title="Chats"
         comment="1 unread"
         button={
-          <div className=" cursor-pointer bg-darkGreen text-primary p-[8px] rounded-md">
+          <div className=" cursor-pointer bg-darkGreen text-primary p-[8px] rounded-md" onClick={() => dispatch(setIsNewChatModal(true))}>
             <RoundPlus />
           </div>
         }
       />
-      <div className="grid grid-cols-7">
+      < div className="grid grid-cols-7" >
         <div className="col-span-2">
           <UsersSidebar />
         </div>
         <div className="col-span-5 border-[2px] border-[#19191a]">
           <ChattingThreadBox />
-          <Input
+          <MsgInput
             isSocial={true}
             focusState={focusState}
             setFocusState={setFocusState}
@@ -60,8 +67,11 @@ const ChatPanel = () => {
             onDrop={() => setToggleDropZone("none")}
           ></div>
         </div>
-      </div>
-    </div>
+      </div >
+      {isNewChatModal && (
+        <NewChatModal />
+      )}
+    </div >
   );
 }
 
