ec28dd53bd7fb5eb27f5869776644d72b689dc9a
shiningsea0415 <ski.britness@gmail.com>
2022-10-27 00:50:28 +0700

    set touchstart

--- src/components/Common/Buttons/PrimaryButton.tsx
+++ src/components/Common/Buttons/PrimaryButton.tsx
@@ -11,7 +11,7 @@ export interface PrimaryButtonProps {
 
 const PrimaryButton = (props: PrimaryButtonProps) => {
   return (
-    <button className={`solarity-button font-medium p-[16px] sm:p-[22px] rounded-[22px] text-white w-[100%] h-[50px] sm:h-[62px] text-[16px] sm:text-[18px] text-center tracking-wider inline-flex items-center justify-center ${props.bordered ? 'text-lightprimary border-lightprimary border-2' : ''} ${props.disabled ? 'bg-[#1d1e20] button-disabled' : 'bg-primary'} ${props.styles}`} onClick={props.disabled ? null : props.onClick}>
+    <button className={`solarity-button font-medium p-[16px] sm:p-[22px] rounded-[22px] text-white w-[100%] h-[50px] sm:h-[62px] text-[16px] sm:text-[18px] text-center tracking-wider inline-flex items-center justify-center ${props.bordered ? 'text-lightprimary border-lightprimary border-2' : ''} ${props.disabled ? 'bg-[#1d1e20] button-disabled' : 'bg-primary'} ${props.styles}`} onClick={props.disabled ? null : props.onClick} onTouchStart={props.disabled ? null : props.onClick}>
       {props.icon ? <i className="fa fa-chrome fa-lg pr-[10px]"></i> : ""}
       <span>{props.caption}</span>
     </button>
