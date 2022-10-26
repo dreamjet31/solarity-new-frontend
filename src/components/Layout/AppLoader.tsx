e29acfe953f4fe68ef0590e3f91fe3508573ed30
shiningsea0415 <ski.britness@gmail.com>
2022-10-26 19:57:53 +0700

    fix app loader

--- src/components/Layout/AppLoader.tsx
+++ src/components/Layout/AppLoader.tsx
@@ -9,7 +9,7 @@ const AppLoader = () => {
   }
 
   return (
-    <div className="fixed z-[1000000000] h-[100vh] w-[100vw] bg-black/80">
+    <div className="fixed z-[1000000000] h-[100vh] w-[100vw] bg-black/80 top-0 bottom-0">
       <div className="absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]">
         <div className="flex flex-col items-center justify-center">
           <div className="h-16 w-16 animate-spin">
