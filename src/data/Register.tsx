92b4e4110894fb255c5a0ab73b1961cc50e502d9
shiningsea0415 <ski.britness@gmail.com>
2022-10-31 03:58:55 +0700

    fix badge image on model

--- src/data/Register.tsx
+++ src/data/Register.tsx
@@ -9,6 +9,6 @@ export const badges : BadgeDataType[] = [
     { icon: '/images/badges/Dev.png', name: 'Dev', active: false },
     { icon: '/images/badges/Diamond hands.png', name: 'Diamond Hands', active: false },
     { icon: '/images/badges/Gamer.png', name: 'Gamer', active: false },
-    { icon: '/images/badges/NFT bro.png', name: 'NDR Bro', active: false },
+    { icon: '/images/badges/NFT bro.png', name: 'NFT Bro', active: false },
     { icon: '/images/badges/UX designer.png', name: 'UX Designer', active: false },
   ]
