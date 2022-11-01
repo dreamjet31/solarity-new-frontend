9210c6cfdfc2b85ad5708f4574e0d3fcbbebd613
memokey <skytopground@gmail.com>
2022-11-01 18:50:38 +0800

    changed

--- src/components/Intro/Slider/index.tsx
+++ src/components/Intro/Slider/index.tsx
@@ -20,7 +20,7 @@ const Slider = (props: SliderType) => {
 
   return (
     <div className="h-[100vh]">
-      <Image src={props.backgroundImage} className="rounded-b-[40px]" width={664} height={664} alt={props.title} />
+      <img src={props.backgroundImage} className="rounded-b-[40px]" width={664} height={664} alt={props.title} />
       <div className="px-12 py-20">
         <h2 className="text-[25px] font-[700] title-color pb-8">{props.title}</h2>
         <p className="center text-[15px] font-[500] title-color pb-12">
