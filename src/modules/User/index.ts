854184267a32ad76febc7f455f88753237d2de81
memokey <skytopground@gmail.com>
2022-11-16 04:07:37 +0800

    changed

--- src/modules/User/index.ts
+++ src/modules/User/index.ts
@@ -12,6 +12,27 @@ export type UserType = {
   ethereumAddress: string;
 };
 
+export async function getStaticPaths() {
+  try {
+    const {
+      data: { roomIds },
+    } = await apiCaller.get(`/users`);
+    const paths = roomIds.map((roomId: string) => ({
+      params: { link: roomId }
+    }))
+    return {
+      paths: paths,
+      fallback: false, // can also be true or 'blocking'
+    }
+  } catch (err) {
+    return {
+      paths: [],
+      fallback: false, // can also be true or 'blocking'
+    }
+  }
+}
+
+
 export const getServerSideProps = async (context: any) => {
   const { username } = context.query;
   try {
