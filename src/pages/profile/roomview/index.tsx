84c23f83bcc79a35faa20096750664ddc838a3e7
memokey <skytopground@gmail.com>
2022-11-17 21:17:45 +0800

    changed

--- src/pages/profile/roomview/index.tsx
+++ src/pages/profile/roomview/index.tsx
@@ -1,12 +1,16 @@
 import React, { FC } from "react"
 import LoadingScr from "components/Experience/RoomInterior/LoadingScr"
 import EditRoom from "components/Profile/EditRoom"
+import { RootStateOrAny, useSelector } from "react-redux"
 
 const ProfileIndex: FC = () => {
+  const { profile } = useSelector((state: RootStateOrAny) => ({
+    profile: state.profile.data,
+  }))
   return (
     <div className="h-full w-full">
       <LoadingScr />
-      <EditRoom />
+      <EditRoom user={profile} />
     </div>
   )
 }
