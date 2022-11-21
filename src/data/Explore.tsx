e0911a184e13ae4445da7f9c4ae2645982065474
memokey <skytopground@gmail.com>
2022-11-21 17:43:33 +0800

    integrated explore banner

---
+++ src/data/Explore.tsx
@@ -0,0 +1,38 @@
+import { SliderType } from "../components/Intro/Slider"
+export const EXPLORE_BANNER_SLIDES: SliderType[] = [
+  {
+    title: 'Aurory SZN is here!',
+    backgroundImage: '/images/community/backs/BG (1).png',
+    content: <div>Damage with Legendary Weapons. If you like to suck dicks.<br />Play now to earn infinite rewards, and claim your airdrop!</div>,
+    button: 'Launch Now',
+    path: 'https://aurory.io/'
+  },
+  {
+    title: 'Portals',
+    backgroundImage: '/images/community/backs/BG (6).png',
+    content: <div>The Metaverse on Solana. Explore downtown, invite friends, chat, build, show off your NFTs — right in the browser.</div>,
+    button: 'Launch Now',
+    path: 'https://theportal.to/'
+  },
+  {
+    title: 'Mini Royale',
+    backgroundImage: '/images/community/backs/BG (11).png',
+    content: <div>War, deception, and destruction ravages the Miniverse. Vikings, channeling the power of Odin, have descended upon the Miniverse to compete in quests, form powerful clans, and seize control. Let the battles begin.</div>,
+    button: 'Launch Now',
+    path: 'https://miniroyale.io'
+  },
+  {
+    title: 'Panzer Dogs',
+    backgroundImage: '/images/community/backs/BG (5).png',
+    content: <div>A PVP tank brawler on Solana starting with the release of 5555 unique dog avatars.</div>,
+    button: 'Launch Now',
+    path: 'https://home.panzerdogs.io/'
+  },
+  {
+    title: 'ev.io',
+    backgroundImage: '/images/community/backs/BG (11).png',
+    content: <div>Ev.io is a play to earn blockchain FPS - equip a character skin, weapon and sword to start earning e (which converts to SOL) for every kill. Equip all 3 to unlock maximum earning potential. Play at https://ev.io!</div>,
+    button: 'Launch Now',
+    path: 'https://ev.io'
+  },
+];
--- src/modules/Explore/ExploreBanner.tsx
+++ src/modules/Explore/ExploreBanner.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
-const ExploreBanner = ({sidebarToggler}) => {
+import { Carousel } from 'react-responsive-carousel'
+import { EXPLORE_BANNER_SLIDES } from 'data/Explore';
+import BannerSlide from 'components/Explore/BannerSlide';
+const ExploreBanner = ({ sidebarToggler }) => {
     return (
-        <></>
+        <Carousel showThumbs={false} showStatus={false} showArrows={false} >
+            {
+                EXPLORE_BANNER_SLIDES.map((slider, index) => (
+                    <BannerSlide {...slider} key={index} />
+                ))
+            }
+        </Carousel>
     )
 }
 
