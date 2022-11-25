cd8f0ae009b1e09c60081202305e78d20344389d
memokey <skytopground@gmail.com>
2022-11-26 03:45:40 +0800

    integrated default banner on experience

--- src/modules/Experience/ExperienceBanner.tsx
+++ src/modules/Experience/ExperienceBanner.tsx
@@ -30,11 +30,25 @@ const ExperienceBanner = () => {
 
   return (
     <div className="w-full h-full relative rounded-[25px] overflow-hidden md:block xs:hidden">
-      <Image
-        src={!!selectedLiveRoom.imageUrl ? selectedLiveRoom.imageUrl : `/images/experience/room_images/room_1.jpg`}
-        layout="fill"
-        alt="Experience Banner Image"
-      />
+      {selectedLiveRoom.imageUrl ? (
+        <Image
+          src={!!selectedLiveRoom.imageUrl ? selectedLiveRoom.imageUrl : `/images/experience/room_images/room_1.jpg`}
+          layout="fill"
+          alt="Experience Banner Image"
+        />
+      ) : (
+        <div className='flex border border-[#1A1A1C]'>
+          <div className='relative w-[50%] h-[496px] bg-[#131314]'>
+            <Image src="/images/intro/slides/hubs.png" layout="fill" />
+          </div>
+          <div className='w-[50%] bg-[#131314] flex items-center justify-center text-center'>
+            <div>
+              <h2 className='text-[30px] font-[700] title-color'>HUBS</h2>
+              <p className='center text-[19px] font-[500] content-color'> Socialize, play, bet and trade in the <br />most immersive way possible.<br />Poker, WL hunting, presentations and more.<br /></p>
+            </div>
+          </div>
+        </div>
+      )}
       {!!selectedLiveRoom.title && (
         <RoomInfoDlg
           selectedLiveRoom={selectedLiveRoom}
