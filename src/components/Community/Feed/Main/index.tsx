55844da6011ee9a65c2baf850d8f2e15f50f5a0f
memokey <skytopground@gmail.com>
2023-01-10 15:42:20 -0800

    changed

--- src/components/Community/Feed/Main/index.tsx
+++ src/components/Community/Feed/Main/index.tsx
@@ -3,38 +3,46 @@ import TabItem from 'components/Common/Forms/TabItem'
 import Feed from './Feed'
 import FeedGallery from './Gallery'
 import FeedMarketplace from './Marketplace'
+import GameContent from 'components/Game/GameContent'
 
 function Main({ id, type, visitRoom, setIsMarketplace, activeIndex = 0, setActiveIndex }) {
     return (
-        <div className='w-full'>
-            <div className={`relative w-fit`}>
-                <div className={`profile-tab flex flex-row mt-8 relative sm:px-[0px] xs:px-[20px]
-                                md:w-full xs:w-[87vw]
-                                border-b-[1px] border-b-semiSplitter overflow-x-scroll scroll-smooth`}>
-                    <TabItem title="Feed" selectedStatus={activeIndex === 0} onClick={() => {
-                        setActiveIndex(0)
-                        setIsMarketplace(false)
-                    }
-                    } />
-                    <TabItem title="Gallery" selectedStatus={activeIndex === 1} onClick={() => {
-                        setActiveIndex(1)
-                        setIsMarketplace(false);
-                    }
-                    } />
-                    <TabItem title="Marketplace" selectedStatus={activeIndex === 2} onClick={() => {
-                        setActiveIndex(2)
-                        setIsMarketplace(true)
-                    }
-                    } />
+        <div>
+            {type != 'game' && (
+                <div className='w-full'>
+                    <div className={`relative w-fit`}>
+                        <div className={`profile-tab flex flex-row mt-8 relative sm:px-[0px] xs:px-[20px]
+                                        md:w-full xs:w-[87vw]
+                                        border-b-[1px] border-b-semiSplitter overflow-x-scroll scroll-smooth`}>
+                            <TabItem title="Feed" selectedStatus={activeIndex === 0} onClick={() => {
+                                setActiveIndex(0)
+                                setIsMarketplace(false)
+                            }
+                            } />
+                            <TabItem title="Gallery" selectedStatus={activeIndex === 1} onClick={() => {
+                                setActiveIndex(1)
+                                setIsMarketplace(false);
+                            }
+                            } />
+                            <TabItem title="Marketplace" selectedStatus={activeIndex === 2} onClick={() => {
+                                setActiveIndex(2)
+                                setIsMarketplace(true)
+                            }
+                            } />
+                        </div>
+                    </div>
+                    {activeIndex === 0 ?
+                        <Feed id={id} type={type} />
+                        : activeIndex === 1 ?
+                            <FeedGallery visitRoom={visitRoom} />
+                            : activeIndex === 2 ?
+                                <FeedMarketplace visitRoom={visitRoom} />
+                                : ""}
                 </div>
-            </div>
-            {activeIndex === 0 ?
-                <Feed id={id} type={type} />
-                : activeIndex === 1 ?
-                    <FeedGallery visitRoom={visitRoom} />
-                    : activeIndex === 2 ?
-                        <FeedMarketplace visitRoom={visitRoom} />
-                        : ""}
+            )}
+            {type == 'game' && (
+                <GameContent />
+            )}
         </div>
     )
 }
