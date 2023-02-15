1894e3ea80477ef47edd01a4d3bfdd2501598c60
memokey <skytopground@gmail.com>
2023-02-15 11:46:30 -0800

    changed

--- src/modules/Quests/index.tsx
+++ src/modules/Quests/index.tsx
@@ -1,9 +1,25 @@
-import React from "react"
+import React, { useEffect, useMemo } from "react"
 import { SubTitle } from "components/Common/Titles";
 import { QuestPanel, QuestGlobalPanel } from "components/Common/Panels";
 import { SOLARITY_QUESTS, SOLARITY_GLOBAL_QUESTS } from 'data/Quest';
+import { RootStateOrAny, useSelector } from "react-redux";
+import { useAsyncMemo } from "use-async-memo";
+import { apiCaller } from "utils/fetcher";
 
 const Quests = ({sidebarToggler}) => {
+
+    const games = useAsyncMemo(async () => {
+        try {
+          const {
+            data: { games }
+          } = await apiCaller.get(`/games`);
+          return games;
+        } catch (error) {
+          console.error('Something went wrong.');
+          return [];
+        }
+      }, []);
+
     return (
         <div className='my-[16px]'>
             <SubTitle title="Solarity Quests" />
@@ -17,13 +33,21 @@ const Quests = ({sidebarToggler}) => {
                 ))}
             </div>
             <SubTitle title="All Quests" />
-            <div className="flex gap-8">
-                {SOLARITY_GLOBAL_QUESTS.map((quest, index) => (
-                    <QuestGlobalPanel
-                    {...quest}
-                        index={index}
-                        onClick={() => {}}
-                    />
+            <div className="">
+                {games && games.map((game, index1) => (
+                    <div className="flex mb-8 gap-8">
+                        {game.quests.map((quest, index2) => (
+                            <QuestGlobalPanel
+                                title={quest.name}
+                                description={quest.detail}
+                                avatar={game.avatarImage}
+                                icon={"/images/wallets/xp.png"}
+                                amount={quest.score}
+                                index={index1 * 4 + index2}
+                                onClick={() => {}}
+                            />
+                        ))}
+                    </div>
                 ))}
             </div>
         </div>
